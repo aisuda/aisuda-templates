9275630534ba69ef46113190a78e1d23acdc9c24
RickCole21 <rickcole21@outlook.com>
2021-05-20 20:31:35 +0800

    fix path

--- pages/crud-card/list.api.js
+++ pages/crud-card/list.api.js
@@ -1,42 +1,42 @@
 const data = [
   {
     title: '爱奇艺',
-    avatar: '/pages/crud-card/iqiyi.jpeg',
+    avatar: '<template_path>/iqiyi.jpeg',
     subDescription: '悅享品質'
   },
   {
     title: '今日头条',
-    avatar: '/pages/crud-card/toutiao.jpeg',
+    avatar: '<template_path>/toutiao.jpeg',
     subDescription: '信息创造价值'
   },
   {
     title: 'Riot Games',
-    avatar: '/pages/crud-card/riot-game.jpeg',
+    avatar: '<template_path>/riot-game.jpeg',
     subDescription: 'STAY HUNGRY; STAY HUMBLE'
   },
   {
     title: 'Starbucks',
-    avatar: '/pages/crud-card/starbucks.jpeg',
+    avatar: '<template_path>/starbucks.jpeg',
     subDescription: 'To inspire and nurture the human spirit'
   },
   {
     title: 'Pizza Hut',
-    avatar: '/pages/crud-card/pizza-hut.jpeg',
+    avatar: '<template_path>/pizza-hut.jpeg',
     subDescription: 'No One Outpizzas the Hut'
   },
   {
     title: '微信',
-    avatar: '/pages/crud-card/wechat.jpeg',
+    avatar: '<template_path>/wechat.jpeg',
     subDescription: '微信，是一个生活方式'
   },
   {
     title: "McDonald's",
-    avatar: '/pages/crud-card/mcdonold.jpeg',
+    avatar: '<template_path>/mcdonold.jpeg',
     subDescription: "I'm lovin' it"
   },
   {
     title: 'Blizzard Entertainment',
-    avatar: '/pages/crud-card/blizzard.jpeg',
+    avatar: '<template_path>/blizzard.jpeg',
     subDescription:
       'Welcome To Blizzard. At Blizzard Entertainment, we pour our hearts and souls into everything we create.'
   }
